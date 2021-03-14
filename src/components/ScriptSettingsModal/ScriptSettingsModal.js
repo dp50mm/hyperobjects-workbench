170d0c9463cc16ee19a622565761dd0472a8cdf8
Erwin Hoogerwoord <erwinhoogerwoord@erwins-mbp-2.home>
2021-03-14 13:48:46 +0100

    added public sharing route to frontend & toggle to control access

--- src/components/ScriptSettingsModal/ScriptSettingsModal.js
+++ src/components/ScriptSettingsModal/ScriptSettingsModal.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import { ScriptContext } from 'App'
+import React, { useContext } from 'react'
 import {
-    Modal
+    Modal,
+    Checkbox,
+    Table,
+    Form,
+    Input
 } from 'semantic-ui-react'
+import _ from "lodash"
+import dayjs from "dayjs"
+
+let latestVersionStored = dayjs().clone().subtract(5, 'seconds')
 
 const ScriptSettingsModal = ({uiState}) => {
-    console.log(uiState.mainPageModal)
+    const scriptsContext = useContext(ScriptContext)
+    console.log(uiState.selectedScript)
+    const script = uiState.selectedScript
+
     return (
         <Modal
             open={uiState.mainPageModal === "script-settings"}
@@ -15,9 +27,47 @@ const ScriptSettingsModal = ({uiState}) => {
             closeIcon
             >
             <Modal.Content>
-                <h1>Script settings</h1>
-                <p>Public toggle</p>
-                <p>Sharing url</p>
+                <h3>Script: {script.name}</h3>
+                <p>Updated at: {script.updatedAt}</p>
+                <div className='ui form'>
+                    <Form.Field>
+                    <Input
+                        style={{width: "90%", marginBottom: 10}}
+                        value={`https://workbench.hyperobjects.design/share/?script=${script._id}`}
+                        label="Share url"
+                        disabled={!_.get(script, 'public', false)}
+                        />
+                    <Checkbox
+                        style={{marginLeft: 10}}
+                        label="public"
+                        checked={_.get(script, 'public', false)}
+                        onChange={() => {
+                            var storeVersion = false
+                            if (dayjs().diff(latestVersionStored, 'second') > 30) {
+                                storeVersion = true
+                                latestVersionStored = dayjs()
+                            }
+                            scriptsContext.actions.updateScript({
+                                ...script,
+                                public: !_.get(script, "public", false),
+                                storeVersion: latestVersionStored
+                            })
+                        }}
+                        />
+                    </Form.Field>
+                </div>
+                {window.location.host.includes("localhost:3000") && (
+                    <div className='ui form'>
+                        <Form.Field>
+                        <Input
+                            style={{width: "90%"}}
+                            value={`http://localhost:3000/share/?script=${script._id}`}
+                            label="Share url"
+                            disabled={!_.get(script, 'public', false)}
+                            />
+                        </Form.Field>
+                    </div>
+                )}
             </Modal.Content>
         </Modal>
     )
--- src/containers/SharePage/SharePage.js
+++ src/containers/SharePage/SharePage.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
+import _ from "lodash"
+import Workbench from 'components/Workbench'
 
 const SharePage = () => {
+    const [loadingScript, setLoadingScript] = useState(false)
+    const [script, setScript] = useState(false)
+    const [loadingError, setLoadingError] = useState(false)
+    
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+
+    const scriptId = urlParams.get("script")
+
+    useEffect(() => {
+        if(!_.isNull(scriptId) && loadingScript === false && script === false) {
+            setLoadingScript(true)
+            var fetchUrl = false
+            if(window.location.hostname.includes('localhost')) {
+                fetchUrl = `http://localhost:5000/script/public/${scriptId}`
+            } else {
+                fetchUrl = `https://api.hyperobjects.design/script/public/${scriptId}`
+            }
+            fetch(fetchUrl)
+                .then(resp => {
+                    if(resp.status === 200) {
+                        resp.json().then(script => {
+                            document.title = _.get(script, 'name', "Hyperobjects script")
+                            setScript(script)
+                        })
+                    } else {
+                        console.log(resp)
+                        setLoadingError(true)
+                    }
+                }).catch(err => {
+                    console.log(err)
+                    setLoadingError(true)
+                })
+        }
+    }, [loadingScript, script, setLoadingScript])
+    
+    if(loadingError) {
+        return (
+            <p>Loading error</p>
+        )
+    }
+    if(script === false) {
+        return (
+            <p>loading script</p>
+        )
+    }
+
     return (
         <div className='share-page'>
-            <p>Loading script</p>
+            <Workbench
+                name={_.get(script, 'name', 'no script name')}
+                script={script.script}
+                scriptFromProp={true}
+                autoRun={true}
+                />
         </div>
     )
 }
